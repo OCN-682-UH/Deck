8179ca1ff3219da96c82a1af862df67e655d76f0
AnnieDeck <adeck@hawaii.edu>
2024-10-08 11:39:06 -1000

    Week 6 homework (RMarkddown)

---
+++ Week_06/Scripts/Week_06_script.R
@@ -0,0 +1,26 @@
+## Week 6a: ntro to RMarkdown 
+
+##Created by: Annie Deck
+
+##Created on: 
+
+##########################################
+
+##look at helpful cheatships and guides on powerpoint slide
+
+##Four major parts: metadata, text, code, output
+
+##everything in between the 3 dashes contains your metadata (must write in mark down language in here)
+##two spaces and then enter to create a new line 
+
+## in the r chunk you type like a normal r script 
+
+##install rmarkdown
+install.packages("rmarkdown")
+install.packages("tinytex")
+
+##donʻt have include=FALSE in your set up or none of you chunks of code will show up and you will have a blank markdown 
+
+install.packages("beepr")
+library("beepr")
+beep(6)
